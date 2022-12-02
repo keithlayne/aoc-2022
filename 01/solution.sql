bf69e870b62087c5740113dacc8cd5966284d56d
Keith Layne <2667202+keithlayne@users.noreply.github.com>
2022-12-02 15:30:27 -0500

    Make things fancier, use schemas

--- 01/solution.sql
+++ 01/solution.sql
@@ -1,7 +1,19 @@
-drop table if exists day1input, day1data;
-create table day1input (raw text);
-\copy day1input (raw) from program 'sed "/./{:a;N;s/\n\(.\)/ \1/;ta}" $(git rev-parse --show-toplevel)/01/input.txt' where raw != '';
-create table day1data as select (select sum(val::int) from regexp_split_to_table(raw,'\s+') val) calories from day1input;
+\set QUIET 1
+\timing off
+SET client_min_messages TO WARNING;
 
-select max(calories) "Part 1" from day1data;
-select sum(top3.calories) "Part 2" from (select calories from day1data order by calories desc limit 3) top3;
+drop schema if exists day01 cascade;
+create schema day01;
+set search_path to day01, public;
+
+drop table if exists input, data;
+create table input (raw text);
+\copy input (raw) from program 'sed "/./{:a;N;s/\n\(.\)/ \1/;ta}" $(git rev-parse --show-toplevel)/01/input.txt' where raw != '';
+create table data as select (select sum(val::int) from regexp_split_to_table(raw,'\s+') val) calories from input;
+
+\timing on
+\unset QUIET
+
+\echo Day 01
+select max(calories) "Part 1" from data;
+select sum(top3.calories) "Part 2" from (select calories from data order by calories desc limit 3) top3;
--- 02/solution.sql
+++ 02/solution.sql
@@ -1,11 +1,25 @@
-drop table if exists day2input, day2part1, day2part2;
-create table day2input (a char, b char);
-\copy day2input from program 'cat $(git rev-parse --show-toplevel)/02/input.txt' delimiter ' ' csv;
+\set QUIET 1
+\timing off
+SET client_min_messages TO WARNING;
 
-create table day2part1 (a char, b char, score int);
-insert into day2part1 (a, b, score) values ('A', 'X', 4), ('A', 'Y', 8), ('A', 'Z', 3), ('B', 'X', 1), ('B', 'Y', 5), ('B', 'Z', 9), ('C', 'X', 7), ('C', 'Y', 2), ('C', 'Z', 6);
-select sum(score) "Part 1" from day2input join day2part1 using (a, b);
+drop schema if exists day02 cascade;
+create schema day02;
+set search_path to day02, public;
 
-create table day2part2 (a char, b char, score int);
-insert into day2part2 (a, b, score) values ('A', 'X', 3), ('A', 'Y', 4), ('A', 'Z', 8), ('B', 'X', 1), ('B', 'Y', 5), ('B', 'Z', 9), ('C', 'X', 2), ('C', 'Y', 6), ('C', 'Z', 7);
-select sum(score) "Part 2" from day2input join day2part2 using (a, b);
+create table input (line text);
+\copy input from program 'cat $(git rev-parse --show-toplevel)/02/input.txt';
+
+create table part1 (line text, score int);
+insert into part1 (line, score) values 
+  ('A X', 4), ('A Y', 8), ('A Z', 3), ('B X', 1), ('B Y', 5), ('B Z', 9), ('C X', 7), ('C Y', 2), ('C Z', 6);
+
+create table part2 (line text, score int);
+insert into part2 values 
+  ('A X', 3), ('A Y', 4), ('A Z', 8), ('B X', 1), ('B Y', 5), ('B Z', 9), ('C X', 2), ('C Y', 6), ('C Z', 7);
+
+\timing on
+\unset QUIET
+
+\echo Day 02
+select sum(score) "Part 1" from input join part1 using (line);
+select sum(score) "Part 2" from input join part2 using (line);
