e4aa242ae58f243c6f9d16883d39c72cc457663b
Keith Layne <2667202+keithlayne@users.noreply.github.com>
2022-12-03 02:00:29 -0500

    Make day 2 bettah

--- 02/solution.sql
+++ 02/solution.sql
@@ -9,17 +9,15 @@ set search_path to day02, public;
 create table input (line text);
 copy input from '/aoc/02/input.txt';
 
-create table part1 (line text, score int);
-insert into part1 (line, score) values 
-  ('A X', 4), ('A Y', 8), ('A Z', 3), ('B X', 1), ('B Y', 5), ('B Z', 9), ('C X', 7), ('C Y', 2), ('C Z', 6);
-
-create table part2 (line text, score int);
-insert into part2 values 
-  ('A X', 3), ('A Y', 4), ('A Z', 8), ('B X', 1), ('B Y', 5), ('B Z', 9), ('C X', 2), ('C Y', 6), ('C Z', 7);
-
 \timing on
 \unset QUIET
 
 \echo Day 02
-select sum(score) "Part 1" from input join part1 using (line);
-select sum(score) "Part 2" from input join part2 using (line);
+
+with part1(line, score) as (
+  values ('A X', 4), ('A Y', 8), ('A Z', 3), ('B X', 1), ('B Y', 5), ('B Z', 9), ('C X', 7), ('C Y', 2), ('C Z', 6)
+) select sum(score) "Part 1" from input join part1 using (line);
+
+with part2(line, score) as (
+  values ('A X', 3), ('A Y', 4), ('A Z', 8), ('B X', 1), ('B Y', 5), ('B Z', 9), ('C X', 2), ('C Y', 6), ('C Z', 7)
+) select sum(score) "Part 2" from input join part2 using (line);
