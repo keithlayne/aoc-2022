ba17d873a785a050a4ef4be3b0532ff7a5052d06
Keith Layne <2667202+keithlayne@users.noreply.github.com>
2022-12-05 17:03:22 -0500

    Day 1: no sed

--- 01/solution.sql
+++ 01/solution.sql
@@ -6,9 +6,8 @@ drop schema if exists day01 cascade;
 create schema day01;
 set search_path to day01, public;
 
-drop table if exists input, data;
-create table input (raw text);
-copy input (raw) from program 'sed "/./{:a;N;s/\n\(.\)/ \1/;ta}" /aoc/01/input.txt' where raw != '';
+create table input (line text);
+copy input from '/aoc/01/input.txt';
 
 \timing on
 \unset QUIET
@@ -17,13 +16,13 @@ copy input (raw) from program 'sed "/./{:a;N;s/\n\(.\)/ \1/;ta}" /aoc/01/input.t
 \echo Day 01
 
 with data as (
-  select (
-    select sum(val::int) from regexp_split_to_table(raw,'\s+') val
-  ) calories from input
-),top3 as (
-  select calories from data order by calories desc limit 3
+  select calories, rank() over (order by calories desc) from (
+    select sum(line::int) calories from (
+      select line, count(1) filter (where line = '') over (rows unbounded preceding) grp from input
+    ) grouped where line != '' group by grp
+  ) ranked
 ) select (
     select max(calories) from data
   ) "Part 1", (
-    select sum(calories) from top3
+    select sum(calories) filter (where rank <= 3) from data
   ) "Part 2";
