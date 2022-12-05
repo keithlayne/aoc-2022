be71391c0d96fdf17f57213d6505423d7c656a39
Keith Layne <2667202+keithlayne@users.noreply.github.com>
2022-12-05 17:23:09 -0500

    Day 4: no sed

--- 04/solution.sql
+++ 04/solution.sql
@@ -6,8 +6,8 @@ drop schema if exists day04 cascade;
 create schema day04;
 set search_path to day04, public;
 
-create table input (a int4range, b int4range);
-copy input from program 'sed -r "s/,/|/;s/([0-9]+)-([0-9]+)/[\1,\2]/g" /aoc/04/input.txt' delimiter '|' csv;
+create table input (line text);
+copy input from '/aoc/04/input.txt';
 
 \timing on
 \unset QUIET
@@ -15,8 +15,12 @@ copy input from program 'sed -r "s/,/|/;s/([0-9]+)-([0-9]+)/[\1,\2]/g" /aoc/04/i
 \echo
 \echo Day 04
 
-select (
-    select count(1) from input where a @> b or a <@ b
+with raw as (
+  select regexp_match(line, '(\d+)-(\d+),(\d+)-(\d+)')::int[] m from input
+), data (a, b) as (
+  select int4range(m[1], m[2], '[]'), int4range(m[3], m[4], '[]') from raw
+) select (
+    select count(1) from data where a @> b or a <@ b
   ) "Part 1", (
-    select count(1) from input where a && b
+    select count(1) from data where a && b
   ) "Part 2";
