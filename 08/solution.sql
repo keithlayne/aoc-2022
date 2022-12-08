34345030eb9877f0b48387c989e30154d77f7b28
Keith Layne <2667202+keithlayne@users.noreply.github.com>
2022-12-08 15:06:32 -0500

    Day 8 with functions

--- 08/solution.sql
+++ 08/solution.sql
@@ -9,11 +9,18 @@ set search_path to day08, public;
 create table input (row serial, line text);
 copy input (line) from '/aoc/08/input.txt';
 
+create function score(trees int[], height int) returns int as $$
+  select coalesce(
+    (select i from generate_subscripts(trees, 1) i where trees[i] >= height limit 1),
+    cardinality(trees)
+  )
+$$ language sql immutable;
+
 \timing on
 \unset QUIET
 
 \echo
-\echo Day 08
+\echo Day 08 (with functions)
 
 with grid (row, col, height) as (
   select row, generate_series(1, char_length(line)), string_to_table(line, null)::int from input
@@ -30,14 +37,7 @@ with grid (row, col, height) as (
     downward as (partition by col order by row rows between current row and unbounded following exclude current row),
     leftward as (partition by row order by col desc rows between current row and unbounded following exclude current row),
     rightward as (partition by row order by col rows between current row and unbounded following exclude current row)
-), visible (trees) as (
-  select count(1) filter (where height > all(u) or height > all(d) or height > all(l) or height > all(r)) from views
-), scores (score) as (
-  select max(
-    coalesce((select i from generate_subscripts(u, 1) as s (i) where u[i] >= height limit 1), cardinality(u)) *
-    coalesce((select i from generate_subscripts(l, 1) as s (i) where l[i] >= height limit 1), cardinality(l)) *
-    coalesce((select i from generate_subscripts(r, 1) as s (i) where r[i] >= height limit 1), cardinality(r)) *
-    coalesce((select i from generate_subscripts(d, 1) as s (i) where d[i] >= height limit 1), cardinality(d))
-  )
-  from views
-) select trees "Part 1", score "Part 2" from visible, scores;
+) select
+    count(1) filter (where height > all(u) or height > all(d) or height > all(l) or height > all(r)) "Part 1",
+    max(score(u, height) * score(d, height) * score(l, height) * score(r, height)) "Part 2"
+  from views;
