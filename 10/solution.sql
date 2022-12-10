da45132e86a8f9cc090d69962d6a2ada4ad8bb6d
Keith Layne <2667202+keithlayne@users.noreply.github.com>
2022-12-10 13:52:57 -0500

    Day 10

---
+++ 10/solution.sql
@@ -0,0 +1,33 @@
+\set QUIET 1
+\timing off
+SET client_min_messages TO WARNING;
+
+drop schema if exists day10 cascade;
+create schema day10;
+set search_path to day10, public;
+
+create table input (num serial, line text);
+copy input (line) from '/aoc/10/input.txt';
+
+\timing on
+\unset QUIET
+
+\echo
+\echo Day 10
+
+with recursive matches (m) as (
+  select regexp_match(line, '^\w+( .*)?')::int[] from input
+), deltas (cycle, delta) as (
+  select row_number() over ()::int, n * coalesce(m[1], 0)
+  from matches, generate_series(0, case when m[1] is null then 0 else 1 end) n
+), cycles (cycle, x, c, r) as (
+  select 1, 1, 0, 0
+  union
+  select cycle + 1, x + delta, cycle % 40, cycle / 40
+  from cycles join deltas using (cycle) where cycle < 240
+), part1 ("Part1") as (
+  select sum(cycle * x) filter (where (cycle - 20) % 40 = 0) from cycles
+), part2 ("Part2") as (
+  select string_agg(case when c between x - 1 and x + 1 then '#' else '.' end, '')
+  from cycles group by r order by r
+) select * from part1, part2;
